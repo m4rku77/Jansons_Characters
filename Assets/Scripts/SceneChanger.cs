5dcb4f7425be0550c35e6e96700f593d0a608cf3
Gerda <gerda.fedotova.sk@lvt.lv>
2024-05-22 14:55:41 +0300

    Ieviesti ScrollView, DropDown, TextBox, u.c

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -7,7 +7,7 @@ public class SceneChanger : MonoBehaviour
 {
     public void ToFirstScene()
     {
-        SceneManager.LoadScene("FirstScene", LoadSceneMode.Single);
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
     }
 
     public void ToSecondScene()
